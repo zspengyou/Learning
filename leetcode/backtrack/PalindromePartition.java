48378032cdcdabbbdaad1db5141a95acca007fda
Jay Zhou <jayzho@amazon.com>
2021-04-07 09:52:02 -0400

    Palindrome partition

---
+++ leetcode/backtrack/PalindromePartition.java
@@ -0,0 +1,12 @@
+package leetcode.backtrack;
+
+import java.util.List;
+
+public class PalindromePartition {
+    class Solution {
+        public List<List<String>> partition(String s) {
+            return null;
+
+        }
+    }
+}
