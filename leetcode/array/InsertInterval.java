92852a799b51a97e70a3c3723fb62ac70e14c8a8
Jay Zhou <jayzho@amazon.com>
2021-04-19 10:07:50 -0400

    update insert interval

--- leetcode/array/InsertInterval.java
+++ leetcode/array/InsertInterval.java
@@ -4,13 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InsertInterval {
+    public static void main(String[] args) {
+        System.out.println("https://leetcode.com/problems/insert-interval/");
+    }
+
     class Solution {
         public int[][] insert(int[][] intervals, int[] newInterval) {
             List<int[]> mergedIntervals = new ArrayList<>();
-            for (int i = 0; i < intervals.length; i++) {
 
+            for (int i = 0; i < intervals.length; i++) {
+                if (i == 0 && newInterval[1] < intervals[i][0] ||
+                            newInterval[0]> intervals[i-1][1] &&newInterval[1] < intervals[i][0] ) {
+                    mergedIntervals.add(newInterval);
+                    mergedIntervals.add(intervals[i]);
+                } else if (i == intervals.length - 1 && newInterval[0] > intervals[i][1] ) {
+                    mergedIntervals.add(intervals[i]);
+                    mergedIntervals.add(newInterval);
+                }else if(newInterval[0]>= intervals[i][0] && newInterval[0] <= intervals[i][1]){
+                    int[] tmp = new int[2];
+                    tmp[0] = intervals[i][0];
+                    tmp[0] = newInterval[1];
+                    while (i < intervals.length && newInterval[1]>= intervals[i][1]){
+                        i++;
+                    }
+                    if(i== intervals.length){
+                        tmp[1] = newInterval[1];
+                    }
+                }
             }
             return null;
         }
+
+
     }
 }
