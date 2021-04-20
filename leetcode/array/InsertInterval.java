90577d0fdd0ad52905e493044ca4d9995f1f4198
Jay Zhou <jayzho@amazon.com>
2021-04-20 10:07:36 -0400

    update insert interval

--- leetcode/array/InsertInterval.java
+++ leetcode/array/InsertInterval.java
@@ -13,28 +13,30 @@ public class InsertInterval {
             List<int[]> mergedIntervals = new ArrayList<>();
 
             for (int i = 0; i < intervals.length; i++) {
-                if (i == 0 && newInterval[1] < intervals[i][0] ||
-                            newInterval[0]> intervals[i-1][1] &&newInterval[1] < intervals[i][0] ) {
+                //newInterval is less than the first interval
+                if (newInterval[1] < intervals[0][0] ||
+                            // newInterval is less than the current interval and greater than previous
+                            i > 0 && newInterval[0] > intervals[i - 1][1] && newInterval[1] < intervals[i][0]) {
                     mergedIntervals.add(newInterval);
                     mergedIntervals.add(intervals[i]);
-                } else if (i == intervals.length - 1 && newInterval[0] > intervals[i][1] ) {
+                    // new interval is greater than last interval
+                } else if (i == intervals.length - 1 && newInterval[0] > intervals[i][1]) {
                     mergedIntervals.add(intervals[i]);
                     mergedIntervals.add(newInterval);
-                }else if(newInterval[0]>= intervals[i][0] && newInterval[0] <= intervals[i][1]){
+                    // new interval is overlap with current interval
+                } else if (newInterval[0] >= intervals[i][0] && newInterval[0] <= intervals[i][1]) {
                     int[] tmp = new int[2];
                     tmp[0] = intervals[i][0];
-                    tmp[0] = newInterval[1];
-                    while (i < intervals.length && newInterval[1]>= intervals[i][1]){
+                    while (i < intervals.length && newInterval[1] >= intervals[i][0]) {
                         i++;
                     }
-                    if(i== intervals.length){
-                        tmp[1] = newInterval[1];
-                    }
+                    tmp[0] = Math.max(newInterval[1], intervals[i - 1][1]);
+                    mergedIntervals.add(tmp);
+                } else {
+                    mergedIntervals.add(intervals[i]);
                 }
             }
-            return null;
+            return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
         }
-
-
     }
 }
