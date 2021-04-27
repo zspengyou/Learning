1c3b069cf04d75d624f80ededdb98377d3ecf859
Jay Zhou <jayzho@amazon.com>
2021-04-27 10:13:56 -0400

    submit insert interval

--- leetcode/array/InsertInterval.java
+++ leetcode/array/InsertInterval.java
@@ -1,43 +1,50 @@
 package leetcode.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InsertInterval {
     public static void main(String[] args) {
         System.out.println("https://leetcode.com/problems/insert-interval/");
+        InsertInterval insertInterval = new InsertInterval();
+        insertInterval.test();
+    }
+
+    private void test() {
+        int[][] intervals = new int[2][];
+        intervals[0] = new int[]{1,3};
+        intervals[1] = new int[]{6,9};
+        int[] newInterval = new int[]{2,5};
+        Solution solution = new Solution();
+        int[][] result = solution.insert(intervals,newInterval);
+        System.out.println("Arrays.toString(result) = " + Arrays.toString(result));
     }
 
     class Solution {
         public int[][] insert(int[][] intervals, int[] newInterval) {
             List<int[]> mergedIntervals = new ArrayList<>();
-            if(intervals.length == 0){
-                mergedIntervals.add(newInterval);
+
+            int index = 0;
+            // current interval end < new interval start
+            while (index < intervals.length && intervals[index][1] < newInterval[0]) {
+                mergedIntervals.add(intervals[index]);
+                index++;
             }
-            for (int i = 0; i < intervals.length; i++) {
-                //newInterval is less than the first interval
-                if (newInterval[1] < intervals[0][0] ||
-                            // newInterval is less than the current interval and greater than previous
-                            i > 0 && newInterval[0] > intervals[i - 1][1] && newInterval[1] < intervals[i][0]) {
-                    mergedIntervals.add(newInterval);
-                    mergedIntervals.add(intervals[i]);
-                    // new interval is greater than last interval
-                } else if (i == intervals.length - 1 && newInterval[0] > intervals[i][1]) {
-                    mergedIntervals.add(intervals[i]);
-                    mergedIntervals.add(newInterval);
-                    // new interval is overlap with current interval
-                } else if (newInterval[0] >= intervals[i][0] && newInterval[0] <= intervals[i][1]) {
-                    int[] tmp = new int[2];
-                    tmp[0] = intervals[i][0];
-                    while (i < intervals.length && newInterval[1] >= intervals[i][0]) {
-                        i++;
-                    }
-                    tmp[1] = Math.max(newInterval[1], intervals[i - 1][1]);
-                    mergedIntervals.add(tmp);
-                    i--;
-                } else {
-                    mergedIntervals.add(intervals[i]);
-                }
+            // current interval end >= new interval start
+            int tmpStart = newInterval[0];
+            int tmpEnd = newInterval[1];
+            while (index < intervals.length && intervals[index][0] <= newInterval[1]) {
+                tmpStart = Math.min(intervals[index][0], tmpStart);
+                tmpEnd = Math.max(intervals[index][1], tmpEnd);
+                index++;
+            }
+            mergedIntervals.add(new int[]{tmpStart, tmpEnd});
+
+            // current interval start > new interval end
+            while (index < intervals.length && intervals[index][0] > newInterval[1]) {
+                mergedIntervals.add(intervals[index]);
+                index++;
             }
             return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
         }
