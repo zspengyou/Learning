0ffc235c1ca22ae650bb8e6cf3390fc04c8a3528
Jay Zhou <jayzho@amazon.com>
2021-04-29 10:27:27 -0400

    sqrt failed

--- leetcode/binarysearch/Sqrt.java
+++ leetcode/binarysearch/Sqrt.java
@@ -1,9 +1,31 @@
 package leetcode.binarysearch;
 
 public class Sqrt {
+    public static void main(String[] args) {
+        System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
+        System.out.println("        System.out.println(\"args = \" + args);\n");
+    }
+
     class Solution {
         public int mySqrt(int x) {
-            return 0;
+            if (x == 0 || x == 1) {
+                return x;
+            }
+
+            int right = x >= (int) Math.pow(2, 16) - 1 ? (int) Math.pow(2, 16) - 1 : x;
+            int left = 1;
+            while (left < right - 1) {
+                int tmp = (left + right) / 2;
+                if (tmp * tmp > x) {
+                    right = tmp;
+                } else if (tmp * tmp < x) {
+                    left = tmp;
+                } else {
+                    return tmp;
+                }
+            }
+
+            return left;
         }
     }
 }
