da9402850d3317d693e2e89944f9ee8292b79241
Jay Zhou <jayzho@amazon.com>
2021-05-04 11:09:40 -0400

    validate binary search tree

--- leetcode/binarytree/ValidateBinarySearchTree.java
+++ leetcode/binarytree/ValidateBinarySearchTree.java
@@ -14,28 +14,98 @@ public class ValidateBinarySearchTree {
             if (root == null) {
                 return false;
             }
-            int left = root.left == null ? Integer.MIN_VALUE : visitLeft(root.left);
-            int right = root.right == null ? Integer.MAX_VALUE : visitRight(root.right);
+            if (root.left == null && root.right == null) {
+                return true;
+            }
+            if (root.left != null && root.right == null) {
+                int left = visitLeft(root.left);
+                if (left >= root.val) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            if (root.left == null && root.right != null) {
+                int right = visitRight(root.right);
+                if (root.val >= right) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            int left = visitLeft(root.left);
+            int right = visitRight(root.right);
             if (left >= root.val || root.val >= right) {
                 return false;
             } else {
                 return true;
             }
+
         }
 
-        private int visitRight(TreeNode node) {
-
-            if (node.left != null) {
-                if (node.left.val >= node.val) {
-                    return Integer.MIN_VALUE;
+        // max from left
+        // if not valid, return Integer.MAX
+        private int visitLeft(TreeNode node) {
+            if (node.left == null && node.right == null) {
+                return node.val;
+            }
+            if (node.left != null && node.right == null) {
+                int left = visitLeft(node.left);
+                if (left >= node.val) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return node.val;
                 }
             }
-            return 0;
+            if (node.left == null && node.right != null) {
+                int right = visitRight(node.right);
+                if (node.val >= right) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return right;
+                }
+            }
+            int left = visitLeft(node.left);
+            int right = visitLeft(node.right);
+            if (left >= node.val || node.val >= right) {
+                return Integer.MAX_VALUE;
+            } else {
+                return right;
+            }
         }
 
-        private int visitLeft(TreeNode node) {
-            return 0;
+        // min from right
+        // if not valid, return Integer.MIN
+        private int visitRight(TreeNode node) {
+            if (node.left == null && node.right == null) {
+                return node.val;
+            }
+            if (node.left != null && node.right == null) {
+                int left = visitRight(node.left);
+                if (left >= node.val) {
+                    return Integer.MIN_VALUE;
+                } else {
+                    return left;
+                }
+            }
+            if (node.left == null && node.right != null) {
+                int right = visitRight(node.right);
+                if (node.val >= right) {
+                    return Integer.MIN_VALUE;
+                } else {
+                    return node.val;
+                }
+            }
+            int left = visitRight(node.left);
+            int right = visitRight(node.right);
+            if (left >= node.val || node.val >= right) {
+                return Integer.MIN_VALUE;
+            } else {
+                return left;
+            }
+
         }
+
 
     }
 }
